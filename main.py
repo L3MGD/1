af53f5c9b7cdde817fa17e6581f118d97a6e01fb
L3MGD <grm8820@gmail.com>
2023-07-14 11:09:56 +0300

    fix

--- main.py
+++ main.py
@@ -18,31 +18,28 @@ async def start(message: types.Message):
         " /sendmusic она прикольная и отправит вам музыку или я могу отправить котика /cat или собаку /dog")
 
 
+async def send_video(chat_id: int, path: str):
+    video = types.InputFile(path)
+    await bot.send_video(chat_id=chat_id, video=video)
+
+
 @dp.message_handler(commands=['sendvideo'])
-async def send_video(message:types.Message):
-    async def send_music(path: str):
-        chat_id = message.chat.id
-        video = types.InputFile(path)
-        await bot.send_video(chat_id=chat_id, video=video)
-        await send_video('phonk.mp4')
-        await message.reply('ссылка на этого крутого человека: https://www.youtube.com/@belkalazyrit4792')
+async def _(message: types.Message):
+    chat_id = message.chat.id
+    await send_video(chat_id, 'phonk.mp4')
 
 
 @dp.message_handler(commands=['sendmusic'])
 async def send_music(message: types.Message):
     x = random.choice([1, 2, 3])
-
-    async def send_video(path: str):
-        chat_id = message.chat.id
-        video = types.InputFile(path)
-        await bot.send_video(chat_id=chat_id, video=video)
+    chat_id = message.chat.id
 
     if x == 1:
-        await send_video("video.mp4")
+        await send_video(chat_id, "video.mp4")
     elif x == 2:
-        await send_video("video (1).mp4")
+        await send_video(chat_id, "video (1).mp4")
     elif x == 3:
-        await send_video("video (2).mp4")
+        await send_video(chat_id, "video (2).mp4")
 
 
 @dp.message_handler(commands=['cat'])
